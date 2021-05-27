d1e7d33cb4ece5292538e2f1713bc29f1570ca89
Zhang Baozhe <baozhezhang@link.cuhk.edu.cn>
2021-05-27 17:34:19 +0800

    First commit!

---
+++ 5.27.py
@@ -0,0 +1,23 @@
+import serial
+
+serial_6 = serial.Serial('COM7', 9600, timeout=0.5)
+
+while True:
+    if serial_6.in_waiting:
+        readbuf = serial_6.read(serial_6.in_waiting)
+        readbuf_str = str(readbuf)
+        angle = ''
+        for i in readbuf_str:
+            if i.isdigit() or i == '.':
+                angle += i
+        angle = float(angle)
+        print(angle)
+
+
+
+        serial_6.write(bytes('adds', encoding='utf-8'))
+
+    
+
+
+            
